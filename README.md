3a57c83275f4a24020cd1f2ae11106d4490e4477
geralt-rivia <geralt.rivia@kakaocorp.com>
2021-12-25 19:03:27 +0900

    README 갱신, 일별 발전량 계산 코드 추가. gitignore추가

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # solar-power-logger
+금비전자 인버터를 사용한 태양광 발전시스템에서, 발전량 데이터를 얻는 코드.
+
+데이터는 MQTT를 이용해 얻고, 이를 위해 Elfin EW11모델을 사용함. (사용법은 여기에서 설명하지 않습니다.)
+인버터로 다음 신호를 전송하면, EW11이 상태값을 publish 해줍니다.
+```7E0101D188```
+>상태 요청은 query.py를 참고합니다.
+
+전송받은 신호를 각 비트값에 따라 파싱해서, 사용합니다.
+>신호 순서 및 의미는, 신재생에너지 표준 프로토콜 가이드라인(v1.3)을 참고(구글검색)
+
+### DB, MQTT 연결을 위해 config디렉토리를 만들고, database.json 과 mqtt.json 파일을 정의해야 합니다.
+>database.json
+```json
+{
+	"host" : "000.000.000.000",
+	"port" : "3307",
+	"database" : "database_name",
+	"user" : "user_id",
+	"password" :"user_password"
+}
+
+```
+>mqtt.json
+```json
+{
+	"host" : "000.000.000.000",
+	"port" : "1883",
+	"user" : "user_id",
+	"password" :"password"
+}
+
+```
--- database.py
+++ database.py
@@ -5,11 +5,19 @@ def save_data(output, accum):
     db = conf.get_database_info()
     charset ='utf8'
     connection = pymysql.connect(host=db["host"], port=int(db["port"]), database=db["database"], user=db["user"], password=db["password"], charset=charset)
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    cursor = connection.cursor()
 
-    sql = "INSERT INTO solar_power_logs (output, accumulated_output) VALUES (%s, %s)"
-    values = (output, accum)
-    cursor.execute(sql, values)
+    select_sql = "SELECT accumulated_output FROM solar_power_logs WHERE DATE(created_at) = DATE(NOW() - INTERVAL 1 DAY) order by id desc limit 1"
+    cursor.execute(select_sql)
+    res = cursor.fetchall()
+    previous_accum = 0
+    try:
+        previous_accum = res[0][0]
+    except IndexError:
+        print("no previous accum data. set to 0")
+
+    insert_sql = "INSERT INTO solar_power_logs (output, accumulated_output, daily_accumulated_output) VALUES (%s, %s, %s)"
+    cursor.execute(insert_sql, (output, accum, accum-previous_accum))
     connection.commit()
-    print("Checked Output: " + str(output) + "Kw, Accumulated Output : " + str(accum) + "Kw.")
+    print("Checked Output: " + str(output) + "Kw, Accumulated Output : " + str(accum) + "Kw. Dailt Accumulated Output : " + str(accum-previous_accum) + "Kw.")
     connection.close()
